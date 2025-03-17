7fd187606dbf56a0df0efbba6b5ef216c24fee41
Jewy09 <jewynah.mae.jaronay@accenture.com>
2025-03-17 16:31:15 +0800

    Created header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { MatButtonModule } from '@angular/material/button';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
