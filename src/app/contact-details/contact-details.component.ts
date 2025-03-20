87c0c4c7c69cb41944569aebecf581ee2ba7bed7
Jewy09 <jewynah.mae.jaronay@accenture.com>
2025-03-20 18:54:50 +0800

    done. Need cleaning

--- src/app/contact-details/contact-details.component.ts
+++ src/app/contact-details/contact-details.component.ts
@@ -3,11 +3,12 @@ import { catchError, Observable, of, switchMap } from 'rxjs';
 import { ContactService } from '../contact-list/contact.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Contact } from '../contact-list/contact.model';
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, Location } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact-details',
-  imports: [AsyncPipe, CommonModule],
+  imports: [AsyncPipe, CommonModule, MatIconModule],
   templateUrl: './contact-details.component.html',
   styleUrl: './contact-details.component.scss'
 })
@@ -18,7 +19,12 @@ export class ContactDetailsComponent {
     private route: ActivatedRoute,
     private contactService: ContactService,
     private router: Router,
+    private location: Location,
   ) {}
+
+  goBack() {
+    this.location.back();
+  }
 
   ngOnInit() {
     const contactId = this.route.snapshot.paramMap.get('id')!;
